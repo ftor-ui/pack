323fa1aee49861e98bd42ec5646c2d4b1d81b731
ftor-ui <52377049+ftor-ui@users.noreply.github.com>
2021-07-25 21:36:01 +0300

    Update pack.c

--- pack.c
+++ pack.c
@@ -29,17 +29,46 @@ void pack(FILE* lpFileFrom, FILE* lpFileTo)
 	char unsigned cBytePrev;
 	int iEof = fscanf(lpFileFrom, "%c", &cBytePrev);
 	char unsigned cByteCurr;
-	char unsigned cCounter = (char)1;
+	char unsigned cCounterDup = (char)129; // 1000.0001 - highest bit, mean that the remaning 7 bit indicate count duplicates next byte
+	char unsigned cCounterNotDup = 0;
+	char unsigned cBufferBytes[127];
 	while(iEof == 1) {
 		iEof = fscanf(lpFileFrom, "%c", &cByteCurr);
 		
-		if (cByteCurr != cBytePrev || (int)cCounter == 255 || iEof != 1) {
-			fprintf(lpFileTo, "%c%c", cCounter, cBytePrev);
-			cBytePrev = cByteCurr;
-			cCounter = 0;
+		if (cByteCurr != cBytePrev && cCounterDup == 129)
+			cCounterNotDup++;
+			
+		else {
+			if (cCounterNotDup > 0) {
+				fprintf(lpFileTo, "%c", cCounterNotDup);
+				for (int i = 0; i < (int)cCounterNotDup; i++)
+					fprintf(lpFileTo, "%c", cBufferBytes[i]);
+			}
+			cCounterNotDup = 0;
+			cCounterDup++;
+		}
+		if (cCounterNotDup == 1) {
+			cBufferBytes[0] = cBytePrev;
+			cBufferBytes[1] = cByteCurr;
+		}
+		else if (cCounterNotDup == 126) {
+			cBufferBytes[cCounterNotDup] = cByteCurr;
+			fprintf(lpFileTo, "%c", cCounterNotDup + 1);
+			for (int i = 0; i < (int)cCounterNotDup + 1; i++)
+				fprintf(lpFileTo, "%c", cBufferBytes[i]);
+			cCounterNotDup = 0;
+			fscanf(lpFileFrom, "%c", &cBytePrev);
+			continue;
+		}
+		else if (cCounterNotDup > 1)
+			cBufferBytes[cCounterNotDup] = cByteCurr;
+		
+		if ( (int)cCounterDup - 128 > 1 && (cByteCurr != cBytePrev || (int)cCounterDup == 255 || iEof != 1)) {
+			fprintf(lpFileTo, "%c%c", cCounterDup - 1, cBytePrev);
+			cCounterDup = 129; // restore the variable
 		}
 		
-		cCounter++;
+		cBytePrev = cByteCurr;
 	}
 }
 
@@ -47,10 +76,19 @@ void unpack(FILE* lpFileFrom, FILE* lpFileTo)
 {
 	char unsigned cByte;
 	char unsigned cCount;
-	fscanf(lpFileFrom, "%c", &cCount);
-	while(fscanf(lpFileFrom, "%c", &cByte) == 1) {
-		for (int i = 0; i < (int)cCount; i++)
+	int iEof = fscanf(lpFileFrom, "%c", &cCount);
+	while(iEof == 1) {
+		iEof = fscanf(lpFileFrom, "%c", &cByte);
+		if (cCount > 128)
+			for (int i = 0; i < cCount - 128; i++)
+				fprintf(lpFileTo, "%c", cByte);
+		else {
 			fprintf(lpFileTo, "%c", cByte);
+			for (int i = 1; i < cCount; i++) {
+				iEof = fscanf(lpFileFrom, "%c", &cByte);
+				fprintf(lpFileTo, "%c", cByte);
+			}
+		}
 		fscanf(lpFileFrom, "%c", &cCount);
 	}
 }
